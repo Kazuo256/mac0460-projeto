971c1c90e49de3bcc7bbab5669f680157bd55b68
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-12-08 18:03:53 -0200

    Relatório: como rodar o programa

--- runTests.sh
+++ runTests.sh
@@ -4,7 +4,7 @@ if [ ! -d logs ]; then
   mkdir logs
 fi
 
-for classifier in bayes_classifier knn_classifier
+for classifier in bayes_classifier knn_classifier dtree_classifier rtrees_classifier
 do
   ./runTestBattery.sh $classifier $adapter &
 done
